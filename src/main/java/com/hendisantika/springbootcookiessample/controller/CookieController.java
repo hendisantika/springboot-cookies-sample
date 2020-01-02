fb46f66f201ffa86ab6786b24ea8d83145e813bd
Hendi Santika <hendisantika@gmail.com>
2020-01-03 06:02:07 +0700

    #3 - Add CookieController

---
+++ src/main/java/com/hendisantika/springbootcookiessample/controller/CookieController.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootcookiessample.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cookies-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/01/20
+ * Time: 06.01
+ */
+@RestController
+public class CookieController {
+}
