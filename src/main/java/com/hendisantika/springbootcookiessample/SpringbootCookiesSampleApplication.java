7e86833fbc01ecdf150ca9748cded13cebf448c1
Hendi Santika <hendisantika@gmail.com>
2020-01-03 06:00:08 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootcookiessample/SpringbootCookiesSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcookiessample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCookiesSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCookiesSampleApplication.class, args);
+	}
+
+}
