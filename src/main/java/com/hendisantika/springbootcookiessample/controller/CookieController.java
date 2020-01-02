321b11218574e35728ce8da79b7c04b1b472a61d
Hendi Santika <hendisantika@gmail.com>
2020-01-03 06:02:59 +0700

    #5 - Add setCookie API in CookieController

--- src/main/java/com/hendisantika/springbootcookiessample/controller/CookieController.java
+++ src/main/java/com/hendisantika/springbootcookiessample/controller/CookieController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-cookies-sample
@@ -18,5 +21,21 @@ public class CookieController {
     @GetMapping("/")
     public String readCookie(@CookieValue(value = "username", defaultValue = "Atta") String username) {
         return "Hey! My username is " + username;
+    }
+
+    @GetMapping("/change-username")
+    public String setCookie(HttpServletResponse response) {
+
+        // create a cookie
+        Cookie cookie = new Cookie("username", "Jovan");
+        cookie.setMaxAge(7 * 24 * 60 * 60); // 7 days
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/"); // global cookie accessible every where
+
+        //add cookie to response
+        response.addCookie(cookie);
+
+        return "Username is changed!";
     }
 }
