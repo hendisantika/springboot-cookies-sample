c27913b9816410973ea6e13f81663c729b1e09dc
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-24 11:09:31 +0700

    #13 - Enable JPA Auditing

--- src/main/java/com/hendisantika/springbootcookiessample/SpringbootCookiesSampleApplication.java
+++ src/main/java/com/hendisantika/springbootcookiessample/SpringbootCookiesSampleApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springbootcookiessample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootCookiesSampleApplication {
 
 	public static void main(String[] args) {
