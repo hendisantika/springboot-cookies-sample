ec1f10bbd20f2dd7f08965362c1e9425e9485112
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-24 11:13:27 +0700

    #14 - Add Visitor Entity

---
+++ src/main/java/com/hendisantika/springbootcookiessample/entity/Visitor.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootcookiessample.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cookies-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/03/20
+ * Time: 11.08
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(EntityListeners.class)
+public class Visitor {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id")
+    private UUID id;
+
+    private String sessionId;
+
+    private String ipAddress;
+
+    @CreatedDate
+    private LocalDateTime createdOn;
+}
